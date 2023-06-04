27e098d732438ca7cba23bd88826e63a7405b3ae
lakshay gupta <lakshaygupta2511@gmail.com>
2023-06-04 22:32:58 +0530

    Leetcode Questions

---
+++ Brainstorming_2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        int[] arr = {6,6,5,54, 69, 90, 5};
+        Arrays.sort(arr);
+        System.out.println(Integer.max(5,69));
+    }
+
+    public static String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        return "";
+    }
+}
